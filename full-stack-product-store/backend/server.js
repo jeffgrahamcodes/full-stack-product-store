7cd35247678d94e6a13c0d3199d02896b6cec258
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2025-11-11 14:36:39 -0500

    Set up server

---
+++ full-stack-product-store/backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+
+app.get('/products', (req, res) => {
+  res.send('Home');
+});
+
+app.listen(5000, () => {
+  connectDB();
+  console.log('Server started at http://localhost:5000...');
+});
