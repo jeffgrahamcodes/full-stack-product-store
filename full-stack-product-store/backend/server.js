33d2d92eb4c59fbe2d696a7884a3af7936a19354
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2025-11-11 17:20:09 -0500

    build api

--- full-stack-product-store/backend/server.js
+++ full-stack-product-store/backend/server.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
+
 import { connectDB } from './config/db.js';
+import productRouter from './routes/product.routes.js';
 
 dotenv.config();
 
 const app = express();
 
-app.get('/products', (req, res) => {
-  res.send('Home');
-});
+app.use(express.json());
+
+app.use('/api/products', productRouter);
 
 app.listen(5000, () => {
   connectDB();
